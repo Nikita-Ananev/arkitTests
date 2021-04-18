5e95f5384be54dcc3caea6ae778982a3ad140ad2
Ananev Nikita <ananev1994@yandex.ru>
2021-04-19 01:44:36 +0300

    123

---
+++ CheckDistance/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  CheckDistance
+//
+//  Created by Никита Ананьев on 18.04.2021.
+//
+
+import UIKit
+import RealityKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    @IBAction func addSphereButtonPressed(_ sender: UIButton) {
+        
+        let tapLocation = self.arView.center
+        let hitTestResults = arView.hitTest(tapLocation, types:.featurePoint)
+        
+        guard let x = hitTestResults.first?.worldTransform.columns.3.x,let y = hitTestResults.first?.worldTransform.columns.3.y, let z = hitTestResults.first?.worldTransform.columns.3.z  else {return}
+        
+        let radius = 0.005
+
+        let entity = ModelEntity(mesh: .generateSphere(radius: Float(radius)))
+
+        let anchor = AnchorEntity(world: [x, y, z])
+        
+        anchor.addChild(entity)
+        arView.scene.anchors.append(anchor)
+    }
+
+}
